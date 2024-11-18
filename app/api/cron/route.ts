77596be1872bfea11bd93d4c1b679ad122c45bcb
Kevin Muñoz <harquin09@gmail.com>
2024-11-18 09:44:32 -0500

    cron

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,29 +2,50 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
 export async function GET(request: NextRequest) {
-  // Define `today` y configura `oneDayBefore` como el día siguiente en UTC
-  const today = new Date();
-  const oneDayBefore = new Date(
-    Date.UTC(
-      today.getUTCFullYear(),
-      today.getUTCMonth(),
-      today.getUTCDate() + 1
-    )
-  );
+  try {
+    // Define la fecha actual y el día siguiente a medianoche en UTC
+    const today = new Date();
+    const nextDayMidnightUTC = new Date(
+      Date.UTC(
+        today.getUTCFullYear(),
+        today.getUTCMonth(),
+        today.getUTCDate() + 1
+      )
+    );
 
-  // Busca usuarios con vencimiento menor o igual a `oneDayBefore` y estado "Vigente"
-  const updatedUsers = await db.user.updateMany({
-    where: {
-      AND: [{ vencimiento: { lte: oneDayBefore } }, { estado: "Vigente" }],
-    },
-    data: {
-      estado: "Inactivo",
-    },
-  });
+    // Actualiza usuarios con vencimiento menor o igual a `nextDayMidnightUTC`
+    const updatedUsers = await db.user.updateMany({
+      where: {
+        AND: [
+          { vencimiento: { lte: nextDayMidnightUTC } },
+          { estado: "Vigente" },
+        ],
+      },
+      data: {
+        estado: "Inactivo",
+      },
+    });
 
-  console.log(`${updatedUsers.count} usuarios actualizados con éxito`);
-  console.log("Hoy (UTC):", today.toISOString());
-  console.log("Un día antes (UTC):", oneDayBefore.toISOString());
+    // Registra información relevante
+    console.log("Hoy (UTC):", today.toISOString());
+    console.log(
+      "Siguiente día a medianoche (UTC):",
+      nextDayMidnightUTC.toISOString()
+    );
+    console.log(
+      `${updatedUsers.count || 0} usuario(s) actualizado(s) con éxito`
+    );
 
-  return NextResponse.json({ message: "Usuarios actualizados con éxito" });
+    // Respuesta
+    return NextResponse.json({
+      message: `${updatedUsers.count || 0} usuario(s) actualizado(s) con éxito`,
+    });
+  } catch (error) {
+    console.error("Error al actualizar usuarios:", error);
+
+    return NextResponse.json(
+      { error: "Ocurrió un error al actualizar usuarios" },
+      { status: 500 }
+    );
+  }
 }
